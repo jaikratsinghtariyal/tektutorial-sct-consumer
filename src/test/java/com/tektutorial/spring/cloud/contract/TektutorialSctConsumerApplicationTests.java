2ef5f1b0e6d50d49b88dca090c03cafa64605967
homeassist <home.assist.code@gmail.com>
2019-05-30 23:57:31 +0530

    adding different case test

--- src/test/java/com/tektutorial/spring/cloud/contract/TektutorialSctConsumerApplicationTests.java
+++ src/test/java/com/tektutorial/spring/cloud/contract/TektutorialSctConsumerApplicationTests.java
@@ -1,5 +1,10 @@
 package com.tektutorial.spring.cloud.contract;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.json.JSONParser;
+import org.assertj.core.api.AssertionsForInterfaceTypes;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +13,8 @@ import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -32,10 +39,26 @@ public class TektutorialSctConsumerApplicationTests {
 
 		RestTemplate restTemplate = new RestTemplate();
 
-		// when:
 		ResponseEntity<String> personResponseEntity =
 				restTemplate.getForEntity("http://localhost:8100/fraudcheck?loanAmount=99999", String.class);
 
-		System.out.println("personResponseEntity:- " + personResponseEntity);
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, String> map = mapper.readValue(personResponseEntity.getBody(), Map.class);
+		Assert.assertEquals(map.get("fraudCheckStatus"), "FRAUD");
+	}
+
+	@Test
+	public void testPositive()
+			throws Exception {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<String> personResponseEntity =
+				restTemplate.getForEntity("http://localhost:8100/fraudcheck?loanAmount=9999", String.class);
+
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, String> map = mapper.readValue(personResponseEntity.getBody(), Map.class);
+		Assert.assertEquals(map.get("fraudCheckStatus"), "Not-Fraud");
+
 	}
 }
